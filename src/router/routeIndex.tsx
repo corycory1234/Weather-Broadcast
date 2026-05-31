c490af196ba6c54bfbd1dcce2246e191c2040f45
Cory <note10litenote10lite@gmail.com>
2026-05-31 20:02:11 +0800

    ci: Amend Changes in Build and Router for Deployment

--- src/router/routeIndex.tsx
+++ src/router/routeIndex.tsx
@@ -2,15 +2,20 @@ import { createBrowserRouter } from "react-router";
 import IndexPage from "../pages/IndexPage";
 import MonthlyPage from "../pages/MonthlyPage";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <IndexPage />,
+    },
+    {
+      path: "/monthly",
+      element: <MonthlyPage />,
+    },
+  ],
   {
-    path: "/",
-    element: <IndexPage />,
-  },
-  {
-    path: "/monthly",
-    element: <MonthlyPage />,
-  },
-]);
+    basename: "/Weather-Broadcast/", // GitHub 自動部屬, 要對齊 repo 英文名稱
+  }
+);
 
 export default router;
